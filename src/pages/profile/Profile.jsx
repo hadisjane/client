e05b0855509e222617f06b8a84a8dad2c5c902bf
Hadis <hadissattorov76@gmail.com>
2024-10-19 22:42:59 +0500

    main

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,8 +3,23 @@ import Topbar from "../../components/topbar/Topbar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 import "./Profile.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function Profile() {
+	const PF = process.env.REACT_APP_PUBLIC_FOLDER
+	const [user, setUser] = useState({});
+	const username = useParams().username;
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const res = await axios.get(`/users?username=${username}`);
+			setUser(res.data);
+		};
+		fetchUser();
+	}, [username]);
+
 	return (
 		<>
 			<Topbar />
@@ -15,23 +30,23 @@ export default function Profile() {
 						<div className="profileCover">
 							<img
 								className="profileCoverImg"
-								src="assets/post/3.jpeg"
+								src={user.coverPicture ? PF + user.coverPicture : PF + "person/noCover.png"}
 								alt=""
 							/>
 							<img
 								className="profileUserImg"
-								src="assets/person/7.jpeg"
+								src={user.profilePicture ? PF + user.profilePicture : PF + "person/noAvatar.png"}
 								alt=""
 							/>
 						</div>
 						<div className="profileInfo">
-							<h4 className="profileInfoName">Fakebook Developer</h4>
-							<span className="profileInfoDesc">Hello my friends!</span>
+							<h4 className="profileInfoName">{user.username}</h4>
+							<span className="profileInfoDesc">{user.desc}</span>
 						</div>
 					</div>
 					<div className="profileRightBottom">
-						<Feed />
-						<Rightbar profile />
+						<Feed username={username} />
+						<Rightbar user={user} />
 					</div>
 				</div>
 			</div>
